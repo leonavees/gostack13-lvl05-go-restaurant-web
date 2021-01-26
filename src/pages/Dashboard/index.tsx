b9a6f8357686ed54e4a1219494718df20ccce5ad
Leonardo <Leonardo@DESKTOP-02UL904>
2021-01-26 12:06:51 -0300

    Desafio 05 GoRestaurant Web

---
+++ src/pages/Dashboard/index.tsx
@@ -0,0 +1,133 @@
+import React, { useState, useEffect } from 'react';
+
+import Header from '../../components/Header';
+
+import api from '../../services/api';
+
+import Food from '../../components/Food';
+import ModalAddFood from '../../components/ModalAddFood';
+import ModalEditFood from '../../components/ModalEditFood';
+
+import { FoodsContainer } from './styles';
+
+interface IFoodPlate {
+    id: number;
+    name: string;
+    image: string;
+    price: string;
+    description: string;
+    available: boolean;
+}
+
+const Dashboard: React.FC = () => {
+    const [foods, setFoods] = useState<IFoodPlate[]>([]);
+    const [editingFood, setEditingFood] = useState<IFoodPlate>(
+        {} as IFoodPlate,
+    );
+    const [modalOpen, setModalOpen] = useState(false);
+    const [editModalOpen, setEditModalOpen] = useState(false);
+
+    useEffect(() => {
+        async function loadFoods(): Promise<void> {
+            const response = await api.get('/foods');
+
+            setFoods(response.data);
+        }
+
+        loadFoods();
+    }, []);
+
+    async function handleAddFood(
+        food: Omit<IFoodPlate, 'id' | 'available'>,
+    ): Promise<void> {
+        try {
+            const response = await api.post('/foods', {
+                ...food,
+                available: true,
+            });
+            const newFood = { ...response.data, available: true };
+            setFoods(oldFoods => [...oldFoods, newFood]);
+        } catch (err) {
+            // eslint-disable-next-line no-alert
+            alert('Erro inesperado ao adicionar comida!');
+        }
+    }
+
+    async function handleUpdateFood(
+        food: Omit<IFoodPlate, 'id' | 'available'>,
+    ): Promise<void> {
+        try {
+            const { id, available } = editingFood;
+            const response = await api.put(`/foods/${id}`, {
+                ...food,
+                available,
+            });
+
+            setFoods(oldFoods =>
+                oldFoods.map(oldFood => {
+                    return oldFood.id === id
+                        ? { ...oldFood, ...response.data }
+                        : oldFood;
+                }),
+            );
+        } catch (err) {
+            // eslint-disable-next-line no-alert
+            alert('Erro ao tentar editar a comida!');
+        }
+    }
+
+    async function handleDeleteFood(id: number): Promise<void> {
+        try {
+            await api.delete(`/foods/${id}`);
+
+            setFoods(oldFoods => oldFoods.filter(f => f.id !== id));
+        } catch (err) {
+            // eslint-disable-next-line no-alert
+            alert('Erro ao tentar remover comida!');
+        }
+    }
+
+    function toggleModal(): void {
+        setModalOpen(!modalOpen);
+    }
+
+    function toggleEditModal(): void {
+        setEditModalOpen(!editModalOpen);
+    }
+
+    function handleEditFood(food: IFoodPlate): void {
+        setEditingFood(food);
+        toggleEditModal();
+    }
+
+    return (
+        <>
+            <Header openModal={toggleModal} />
+            <ModalAddFood
+                isOpen={modalOpen}
+                setIsOpen={toggleModal}
+                handleAddFood={handleAddFood}
+            />
+            <ModalEditFood
+                isOpen={editModalOpen}
+                setIsOpen={toggleEditModal}
+                editingFood={editingFood}
+                handleUpdateFood={handleUpdateFood}
+            />
+
+            <FoodsContainer data-testid="foods-list">
+                {foods &&
+                    foods.map(food => (
+                        <Food
+                            key={food.id}
+                            food={food}
+                            handleDelete={handleDeleteFood}
+                            handleEditFood={handleEditFood}
+                        />
+                    ))}
+            </FoodsContainer>
+        </>
+    );
+};
+
+export default Dashboard;
